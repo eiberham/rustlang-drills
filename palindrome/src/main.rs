818a4a8dfc080eeb79f981ff7e81da560cf86e82
Abraham <cedenoabraham@gmail.com>
2020-12-14 07:26:27 -0300

    palindrome algorithm

--- palindrome/src/main.rs
+++ palindrome/src/main.rs
@@ -1,20 +1,26 @@
 use std::io;
 
-fn main() {
-    println!("Check if a word is palindrome");
+fn main() -> () {
+    println!("Check if a word is palindrome.");
     println!("Please enter a word: ");
 
     let mut input = String::new();
     io::stdin().read_line(&mut input).expect("Error message");
-    let valid: bool = is_palindrome(input);
+
+    // we pass in a reference of input string to avoid ownership issues.
+    let valid: bool = is_palindrome(&input);
 
     if valid {
-        println!("{} is palindrome !");
+        println!("{} is palindrome !", input);
     } else {
-        println!("{} is not palindrome!");
+        println!("{} is not palindrome !", input);
     }
 }
 
+// word is a reference to a string so its value won't get dropped when out 
+// of scope.
 fn is_palindrome(word: &String) -> bool {
-
+    // we need to trim the input string because of the way read_line formats it.
+    let reversed = word.trim().chars().rev().collect::<String>();
+    *word.trim() == reversed
 }
