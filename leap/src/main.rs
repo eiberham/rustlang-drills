acc0f5974d7e505ce554a99616ad446f833a9a31
Abraham <cedenoabraham@gmail.com>
2020-12-16 03:12:43 -0300

    leap year algorithm

--- leap/src/main.rs
+++ leap/src/main.rs
@@ -8,10 +8,31 @@ fn main() -> () {
 
     let mut input = String::new();
 
-    io::stdin().read_line(&mut input).expect("Error message");
+    // Reminder.
+    // stdin reads the global buffer that handles the input stream and 
+    // also implements the BufRead trait which has the read_line method. 
+    // This takes a mutable String as an input buffer and reads all bytes from 
+    // the stream until a newline byte is reached and appends them to the buffer. 
+    // The #expect() method unwraps the Result; if it is an Err it will panic 
+    // with the message and the cause.
+    io::stdin().read_line(&mut input).expect("Couldn't read input!");
 
     if validate_year(&input) {
+        let year_input = input.trim().parse::<u64>();
 
+        let year = match year_input {
+            Ok(year) => year,
+            Err(e) => {
+                println!("Please input a number ({})", e);
+                return;
+            }
+        };
+
+        if is_leap(year) {
+            println!("Year {} is leap", year);
+        } else {
+            println!("Year {} is not leap", year);
+        }
     } else {
         println!("Year format not valid.");
     }
@@ -19,9 +40,9 @@ fn main() -> () {
 
 fn validate_year(year: &String) -> bool {
     let re = Regex::new(r"\d{4}").unwrap();
-    re.is_match(*year)
+    re.is_match(&*year)
 }
 
-fn is_leap(year: &String) {
-
+fn is_leap(year: u64) -> bool {
+    (year % 4 == 0 && year % 100 != 0) || year % 400 == 0
 }
