a3beae76d54d932b2caf8dece621c723d5ffe02a
Abraham <cedenoabraham@gmail.com>
2020-12-29 23:55:41 -0300

    dog initial commit

---
+++ dog/src/main.rs
@@ -0,0 +1,40 @@
+// To get in touch with how structs work in rust, make a program that asks 
+// user to input information about his animal (dog), keep this information 
+// stored in a struct type and add methods to mimic a dog's behaviour.
+
+use std::io;
+
+struct Dog {
+    name: String,
+    age: u32,
+    alive: bool,
+}
+
+fn main() {
+    println!("Welcome! Please answer the following questions about ur dog.");
+    println!("Enter your dog's name: ");
+
+    let mut name = String::new();
+    io::stdin().read_line(&mut name).expect("Couldn't read input!");
+
+    println!("Enter your dog's age: ");
+
+    let mut age = String::new();
+    io::stdin().read_line(&mut age).expect("Couldn't read input!");
+
+    let lifetime = match age.trim().parse::<u32>() {
+        Ok(lifetime) => lifetime,
+        Err(e) => {
+            println!("Please input a valid age ({})", e);
+            return;
+        }
+    };
+
+    let doggy = Dog {
+        name,
+        age: lifetime,
+        alive: true
+    };
+
+
+}
