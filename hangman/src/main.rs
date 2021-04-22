2cec886d5fbba34a97a4776ab0e7221e9d85bc3c
Abraham <cedenoabraham@gmail.com>
2021-04-22 01:16:00 -0300

    updating initial code

--- hangman/src/main.rs
+++ hangman/src/main.rs
@@ -1,15 +1,37 @@
 use console_engine::screen::Screen;
+use console_engine::ConsoleEngine;
 use console_engine::Color;
+use console_engine::KeyCode;
 
 fn main() {
     const SCREEN_WIDTH: u32 = 50;
     const SCREEN_HEIGHT: u32 = 20;
+    const TARGET_FPS: u32 = 10;
+
+    let mut engine = ConsoleEngine::init(SCREEN_WIDTH, SCREEN_HEIGHT, TARGET_FPS);
 
     let mut screen = Screen::new(SCREEN_WIDTH, SCREEN_HEIGHT);
 
-    screen.clear();
+    screen.print_fbg(
+        (screen.get_width() as i32 / 2) / 2 , 
+        2, 
+        "The Hangman - Guess or die",
+        Color::Red, 
+        Color::Reset
+    );
 
-    screen.print_fbg(11, 4, "The Hangman - By Eiberham", Color::Red, Color::Reset);
+    // print the game screen
+    engine.print_screen(1, 0, &screen);    
 
-    screen.draw();
+    loop {
+        engine.wait_frame();
+        // engine.clear_screen(); // reset the screen
+
+        if engine.is_key_pressed(KeyCode::Char('q')) { // if the user presses 'q' :
+            break; // exits app
+        }
+
+        engine.draw(); // draw the screen
+    };
+
 }
