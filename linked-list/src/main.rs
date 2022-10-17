ead6aefd0d8b503aab8c6d89d1fdf876ef9120b0
Abraham <cedenoabraham@gmail.com>
2022-10-16 21:18:19 -0300

    linked list update

--- linked-list/src/main.rs
+++ linked-list/src/main.rs
@@ -3,62 +3,118 @@
 //! There can only be one owner at a time.
 //! When the owner goes out of scope, the value will be dropped.
 
-use std::fmt;
-use std::fmt::Display;
+use std::fmt::{ self, Display };
 use std::rc::Rc;
 use std::cell::RefCell;
+use core::fmt::{ Debug };
 
 #[derive(Debug)]
-struct Node<T> {
-    value: T,
-    next: Option<Rc<RefCell<Node<T>>>>
-}
-
-impl<T> Node<T> {
-    pub fn new(value: T) -> Self {
-        Node {
-            value,
-            next: None
-        }
+enum List<T> {
+    None,
+    Child {
+        value: T,
+        next: Option<Rc<RefCell<List<T>>>>
     }
 }
 
-impl<T: Display> fmt::Display for Node<T> {
+impl<T: Display> fmt::Display for List<T> where T: Debug {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        write!(f, "{}", self.value)
-    }
-}
-
-#[derive(Debug)]
-struct List<T> {
-    element: Node<T>,
-}
-
-impl<T> List<T> {
-    pub fn new(element: Node<T>) -> Self {
-        List {
-            element
-        }
-    }
-
-    pub fn push(&mut self, element: Node<T>) -> () {
-        match self.element.next {
-            Some(_) => (),
-            None => {
-                self.element.next = Some(Rc::new(RefCell::new(element)))
-            }
-        }
-    }
-}
-
-impl<T: Display> fmt::Display for List<T> {
-    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        match self.element.next {
-            Some(_) => {
-                println!("{}", self.element.value);
+        match self {
+            Self::Child { next: _, value } => {
+                println!("value in list is {:?}", value);
                 Ok(())
             },
-            None => Ok(())
+            Self::None => Ok(())
+        }
+    }
+}
+
+impl<T> List<T> where
+    T: Debug + Display + Copy {
+    /// Returns an empty instance of the list
+    pub fn new() -> Self {
+        Self::None
+    }
+
+    /// Adds a new child to the linked list.
+    /// Takes element of any type and then inserts it as the last child node
+    pub fn push(&mut self, element: T) -> () {
+        match self {
+            Self::Child { value: _, next } => { // Use case when it has one or more child
+                // if next is different that none
+                match next {
+                    Some(item) => {
+                        let mut_s = &mut *item.borrow_mut();
+                        mut_s.push(element);
+                    },
+                    None => {
+                        // Add to the next field a new child
+                        let link = Self::Child {
+                            value: element,
+                            next: Some(
+                                Rc::new(
+                                    RefCell::new(List::new())
+                                )
+                            )
+                        };
+                        *next = Some(Rc::new(RefCell::new(link)))
+                    }
+                }
+            }
+            Self::None => { // Use case when the linked list is empty
+                *self = Self::Child {
+                    value: element,
+                    next: None
+                };
+            },
+        }
+    }
+
+    pub fn pop(&mut self) -> () {
+        match self {
+            // Child { value: 5, next: Some(RefCell { value: Child { value: 6, next: Some(RefCell { value: None }) } }) }
+            Self::Child { value: _, next } => {
+                match next {
+                    Some(item) => {
+                        let tail = &mut *item.borrow_mut();
+
+                        // recursively call pop only under the condition where tail.next has some value on it.
+
+                        println!("tail {:?}", tail);
+
+                        match tail {
+                            Self::Child { value, next } => {
+                                match next {
+                                    Some(link) => {
+                                        println!("it has a value, look: {:?}", link);
+                                        let mut_s = &mut *link.borrow_mut(); // this is retrieving List<T>
+                                        // I must access the value field from the enum's Child
+
+                                        // madness here
+                                        match mut_s {
+                                            Self::Child { value, next } => {
+                                                match next {
+                                                    None => mut_s.pop(), //tail.pop()
+                                                    _ => ()
+                                                }
+                                            },
+                                            _ => ()
+                                        }
+                                    }
+                                    None => ()
+                                }
+                            },
+                            _ => ()
+                        }
+                    }
+                    _ => ()
+                }
+            }
+            Self::None => {
+                println!("delete the god damned node");
+                // deletes the node
+                *self = Self::None
+            }
         }
     }
 }
@@ -68,11 +124,15 @@ impl<T: Display> fmt::Display for List<T> {
 fn main() {
     println!("Linked list in rust");
 
-    let node: Node<i32> = Node::new(**Rc::new(RefCell::new(5).borrow_mut()));
-    let mut list: List<i32> = List::new(node);
+    let mut list = List::new();
+    list.push(1);
+    list.push(2);
+    list.push(3);
+    list.push(4);
+    list.push(5);
+    list.push(6);
 
-    let value : Node<i32> = Node::new(**Rc::new(RefCell::new(8).borrow_mut()));
-    list.push(value);
+    list.pop();
 
-    println!("{}", list);
+    println!("{:?}", list);
 }
